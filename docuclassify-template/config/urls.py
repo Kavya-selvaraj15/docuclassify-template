fe3f3aae7e2f9cd02fbf374dee3fc38ae682b4dc
kavya-selvaraj15 <kavyaselvaraj915@gmail.com>
2026-07-05 00:10:37 +0530

    Serve media files in production for demo purposes

--- docuclassify-template/config/urls.py
+++ docuclassify-template/config/urls.py
@@ -1,8 +1,8 @@
-from django.contrib import admin
 from django.urls import path, include
+from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.conf import settings
 from django.conf.urls.static import static
-from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -12,5 +12,5 @@ urlpatterns = [
     path("documents/", include("apps.documents.urls")),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
